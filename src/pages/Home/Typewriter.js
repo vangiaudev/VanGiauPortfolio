0798e9ee4cfd9d45257628a38892c5bbcef4959c
Nguyễn Văn Giàu <vangiau.dev@gmail.com>
2022-11-22 16:13:11 +0700

    Update Profile

--- src/pages/Home/Typewriter.js
+++ src/pages/Home/Typewriter.js
@@ -5,7 +5,7 @@ import styles from './Home.module.scss';
 const cx = classNames.bind(styles);
 const Typewriter = () => {
     const { text } = useTypewriter({
-        words: ['Front End Developer'],
+        words: ['Fullstack Developer'],
         loop: 0,
         typeSpeed: 180,
     });
