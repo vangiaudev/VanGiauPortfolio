13356a8c955b72bab43b3dd8ef245c4b288e7171
Nguyễn Văn Giàu <75024999+vangiaudev@users.noreply.github.com>
2023-07-16 17:00:49 +0700

    Update Typewriter.js

--- src/pages/Home/Typewriter.js
+++ src/pages/Home/Typewriter.js
@@ -5,7 +5,7 @@ import styles from './Home.module.scss';
 const cx = classNames.bind(styles);
 const Typewriter = () => {
     const { text } = useTypewriter({
-        words: ['Back End Developer'],
+        words: ['Software Engineer'],
         loop: 0,
         typeSpeed: 180,
     });
